5f151a109d082847e340f09c3e799aeddc21fa8a
Sparki29 <32461980+Sparki29@users.noreply.github.com>
2017-11-10 20:15:29 +0200

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Person[] arrayOfPersons = new Person[10];
+
+            InitializePersons(arrayOfPersons);
+
+            ListMyArray(arrayOfPersons);
+
+            Console.ReadLine();
+        }
+
+        private static Person CreatePerson()
+        {
+            Person newPerson = new Person();
+
+            Console.Write("First name: ");
+            newPerson.FirstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            newPerson.LastName = Console.ReadLine();
+            return newPerson;
+        }
+
+        private static void InitializePersons(Person[] arrayOfPersons)
+        {
+            Person p1 = new Person();
+            p1.FirstName = "Thomas";
+            p1.LastName = "Anderson";
+
+            arrayOfPersons[0] = new Person { FirstName = "Thomas", LastName = "Anderson" };
+            arrayOfPersons[1] = new Person { FirstName = "Trinity", LastName = "Anderson" };
+            arrayOfPersons[2] = new Person { FirstName = "Little", LastName = "Tom" };
+            arrayOfPersons[3] = CreatePerson();
+        }
+
+        private static void ListMyArray(Person[] arrayOfPersons)
+        {
+            foreach (Person p1 in arrayOfPersons)
+            {
+                if (p1 != null)
+                {
+                    Console.WriteLine("{0} {1}", p1.FirstName, p1.LastName);
+                }
+            }
+        }
+        
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Person> personStack = new Stack<Person>();
+            personStack.Push(CreatePerson());
+            personStack.Push(CreatePerson());
+            personStack.Push(CreatePerson());
+
+            Console.WriteLine();
+            Console.WriteLine("Elements in stack: {0}", personStack.Count);
+            Console.WriteLine();
+
+            foreach (Person p in personStack)
+            {
+                Console.WriteLine("{0} {1}", p.FirstName, p.LastName);
+            }
+
+            Console.WriteLine();
+            Person outPerson = personStack.Pop();
+            Console.WriteLine("{0} {1} is out", outPerson.FirstName, outPerson.LastName);
+            Console.WriteLine();
+            Console.WriteLine("Elements in stack: {0}", personStack.Count);
+            Console.WriteLine();
+
+            foreach (Person p in personStack)
+            {
+                Console.WriteLine("{0} {1}", p.FirstName, p.LastName);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Person CreatePerson()
+        {
+            Person newPerson = new Person();
+
+            Console.Write("First name: ");
+            newPerson.FirstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            newPerson.LastName = Console.ReadLine();
+            return newPerson;
+        }
+    }
+}
