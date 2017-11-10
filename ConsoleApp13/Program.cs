07476ee11a6f958af4b58d7403341ba8dd933bb2
Sparki29 <32461980+Sparki29@users.noreply.github.com>
2017-11-10 20:14:05 +0200

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+
+            char[] Dobromir = Console.ReadLine().ToCharArray();
+
+            foreach (char a in Dobromir)
+            {
+                if (a == 'A'|| a == 'B'|| a == 'C')
+                {
+                 result = result + '2';
+                }
+                if (b == 'D'|| b =='E'|| b == 'F')
+                {
+                    result = result + '3';   //Not finished need to find out how to make the rest of the alphabet
+                    //with different string in the same foreach()
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:");
+
+            string name = Console.ReadLine();
+
+            Console.Write("Please enter your age: ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("What is the year: ");
+
+            int year = int.Parse(Console.ReadLine());
+
+            int bornIn = year - age;
+
+            Console.Clear();
+
+            Console.WriteLine("Hello {0} ! You were born in {1} .",name,bornIn);
+
+            Console.ReadLine();
+        }
+    }
+}
